7f2323e7020ce4d106129d94b5045b9fafb71ec0
Stephen <Stephen@8e687404-24dc-11de-a087-198e6c7179d9>
2006-11-17 07:59:02 +0000

    - Beginnings of Maximize - Minor optimizations

--- trunk/External/UI.js
+++ trunk/External/UI.js
@@ -39,7 +39,9 @@ if (typeof(JsonFx.UI) == "undefined") {
 };
 
 /*string*/ JsonFx.UI.GetStyle = function(/*elem*/ el, /*string*/ style) {
-	el = typeof(el)=="string" ? document.getElementById(el) : el;
+	if (typeof(el)=="string") {
+		el = document.getElementById(el);
+	}
 	if (window.getComputedStyle) {
 		// W3C DOM equivalent to currentStyle:
 		return document.defaultView.getComputedStyle(el, null).getPropertyValue(style);
@@ -49,6 +51,72 @@ if (typeof(JsonFx.UI) == "undefined") {
 		return el.currentStyle[style];
 	}
 	return null;
+};
+
+/*-----------------*\
+	Maximize Verb
+\*-----------------*/
+
+/*function*/ JsonFx.UI.Maximize = function(/*element or string*/ elem, /*JsonFx.UI.Dir*/ dir) {
+
+	if (typeof(elem) == "string") {
+		// either DOM element or id
+		elem = document.getElementById(elem);
+	}
+
+	if (!elem) {
+		return null;
+	}
+
+	var es = elem.style,
+	/*bool*/ state = false;
+
+	var userShape;
+
+	/*void*/ function saveShape() {
+		userShape = {};
+		for (var s in es) {
+			if (/*es.hasOwnProperty(s) &&*/ es[s]) {
+				userShape[s] = es[s];
+			}
+		}
+	}
+	/*void*/ function restoreShape() {
+		if (userShape) {
+			for (var s in userShape) {
+				if (userShape.hasOwnProperty(s)) {
+					es[s] = userShape[s];
+				}
+			}
+		}
+	}
+
+	// state: true = maixmize, false = restore
+	return /*void*/ function max(/*bool*/ newState) {
+
+		if (!state) {
+			saveShape();
+		}
+		// store the latest requested state
+		if (typeof(newState) == "boolean") {
+			state = newState;
+		} else {
+			state = !state;
+		}
+
+		if (state) {
+			es.floatStyle = "none";
+			es.position = "absolute";
+			es.width = JsonFx.UI.GetStyle(document.documentElement, "width");
+			es.height = JsonFx.UI.GetStyle(document.documentElement, "height");
+			es.top = "0px";
+			es.left = "0px";
+			es.bottom = "0px";
+			es.right = "0px";
+		} else {
+			restoreShape();
+		}
+	};
 };
 
 /*-----------------*\
@@ -65,8 +133,6 @@ JsonFx.UI.Dir = {
 	"isVert": function(dir) { return (JsonFx.UI.Dir.Vert&dir)!==0; }
 };
 
-// elem: DOM element or ID
-// dir: true = horizontal, false = vertical
 /*function*/ JsonFx.UI.Minimize = function(/*element or string*/ elem, /*JsonFx.UI.Dir*/ dir) {
 
 	if (typeof(elem) == "string") {
@@ -78,7 +144,8 @@ JsonFx.UI.Dir = {
 		return null;
 	}
 
-	var /*bool*/ mutex = false,
+	var es = elem.style,
+	/*bool*/ mutex = false,
 	/*bool*/ state = false,
 	/*const float*/ StepMin = 0.0,
 	/*const float*/ StepMax = 1.0,
@@ -87,9 +154,9 @@ JsonFx.UI.Dir = {
 	var userOverflow, userHeight, userWidth, pxHeight, pxWidth;
 
 	/*void*/ function saveShape() {
-		userOverflow = elem.style.overflow;
-		userHeight = elem.style.height;
-		userWidth = elem.style.width;
+		userOverflow = es.overflow;
+		userHeight = es.height;
+		userWidth = es.width;
 		pxHeight = typeof(elem.offsetHeight) != "undefined" ?
 				elem.offsetHeight : parseFloat(JsonFx.UI.GetStyle(elem, "height"));
 		pxWidth = typeof(elem.offsetWidth) != "undefined" ?
@@ -114,31 +181,31 @@ JsonFx.UI.Dir = {
 		if (state) {
 			saveShape();
 		}
-		elem.style.display = "block";
+		es.display = "block";
 
 		// minimizeStep
 		/*void*/ function m(/*float*/ step) {
 
 			if (step < StepMin || step > StepMax) {
 				if (state) {
-					elem.style.display = "none";
+					es.display = "none";
 				}
-				elem.style.overflow = userOverflow;
-				elem.style.width = userWidth;
-				elem.style.height = userHeight;
+				es.overflow = userOverflow;
+				es.width = userWidth;
+				es.height = userHeight;
 				mutex = false;
 				return;
 			}
 
-			elem.style.overflow = "hidden";
+			es.overflow = "hidden";
 
 			if (JsonFx.UI.Dir.isHorz(dir)) {
 				// horizontal, simplified lerp
-				elem.style.width = Math.floor(pxWidth*step)+"px";
+				es.width = Math.floor(pxWidth*step)+"px";
 			}
 			if (JsonFx.UI.Dir.isVert(dir)) {
 				// vertical, simplified lerp
-				elem.style.height = Math.floor(pxHeight*step)+"px";
+				es.height = Math.floor(pxHeight*step)+"px";
 			}
 
 			setTimeout(function() {
