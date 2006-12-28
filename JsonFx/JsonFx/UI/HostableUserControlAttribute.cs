52bdf70f2aaf23689e3d26fb7fd5e2ba7508742f
Stephen McKamey <mckamey@users.noreply.github.com>
2006-12-28 15:59:07 +0000

    - added ability to specify control ID on hosted usercontrol so it to matches the original page

--- JsonFx/JsonFx/UI/HostableUserControlAttribute.cs
+++ JsonFx/JsonFx/UI/HostableUserControlAttribute.cs
@@ -8,16 +8,48 @@ namespace JsonFx.UI
 	[AttributeUsage(AttributeTargets.Class, AllowMultiple=false)]
 	public sealed class HostableUserControlAttribute : Attribute
 	{
+		#region Constants
+
+		private const string DefaultID = "_";
+
+		#endregion Constants
+
+		#region Fields
+
+		private string userControlID; 
+
+		#endregion Fields
+
 		#region Init
 
 		/// <summary>
 		/// Ctor.
 		/// </summary>
-		public HostableUserControlAttribute()
+		public HostableUserControlAttribute() : this(HostableUserControlAttribute.DefaultID)
 		{
 		}
 
+		/// <summary>
+		/// Ctor.
+		/// </summary>
+		public HostableUserControlAttribute(string userControlID)
+		{
+			if (String.IsNullOrEmpty(userControlID))
+				this.userControlID = HostableUserControlAttribute.DefaultID;
+			else
+				this.userControlID = userControlID;
+		}
+
 		#endregion Init
+
+		#region Properties
+
+		public string UserControlID
+		{
+			get { return this.userControlID; }
+		}
+
+		#endregion Properties
 
 		#region Methods
 
@@ -32,6 +64,23 @@ namespace JsonFx.UI
 				return false;
 
 			return HostableUserControlAttribute.IsDefined(control.GetType(), typeof(HostableUserControlAttribute));
+		}
+
+		/// <summary>
+		/// Gets a value which indicates if should be ignored in Json serialization.
+		/// </summary>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		public static string GetUserControlID(System.Web.UI.Control control)
+		{
+			if (control == null)
+				return HostableUserControlAttribute.DefaultID;
+
+			HostableUserControlAttribute attribute = HostableUserControlAttribute.GetCustomAttribute(control.GetType(), typeof(HostableUserControlAttribute)) as HostableUserControlAttribute;
+			if (attribute == null)
+				return HostableUserControlAttribute.DefaultID;
+
+			return attribute.UserControlID;
 		}
 
 		#endregion Methods
