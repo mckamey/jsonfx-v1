176c45b34cdf65f526901e2a9cda8eff089cbb7d
Stephen McKamey <mckamey@users.noreply.github.com>
2007-03-01 11:15:14 +0000

    - bringing CSS UserAgent code into JsonFx

--- JsonFx/JsonFx/Build.bat
+++ JsonFx/JsonFx/Build.bat
@@ -7,14 +7,14 @@ If "%2" == "" GOTO HANDLE_ERROR
 %1ScriptCompactor\ScriptCompactor.exe "%2Scripts\Core.js" "%2Scripts\Compacted\Core.js" "Copyright (c)2006-2007 Stephen M. McKamey. http://JsonFx.net"
 %1ScriptCompactor\ScriptCompactor.exe "%2Scripts\IO.js" "%2Scripts\Compacted\IO.js" "Copyright (c)2006-2007 Stephen M. McKamey. http://JsonFx.net"
 %1ScriptCompactor\ScriptCompactor.exe "%2Scripts\UI.js" "%2Scripts\Compacted\UI.js" "Copyright (c)2006-2007 Stephen M. McKamey. http://JsonFx.net"
-%1ScriptCompactor\ScriptCompactor.exe "%2Scripts\Utility.js" "%2Scripts\Compacted\Utility.js" "Copyright (c)2006-2007 Stephen M. McKamey. http://JsonFx.net"
 %1ScriptCompactor\ScriptCompactor.exe "%2Scripts\UA.js" "%2Scripts\Compacted\UA.js" "Copyright (c)2006-2007 Stephen M. McKamey. http://JsonFx.net"
+%1ScriptCompactor\ScriptCompactor.exe "%2Scripts\Utility.js" "%2Scripts\Compacted\Utility.js" "Copyright (c)2006-2007 Stephen M. McKamey. http://JsonFx.net"
 %1ScriptCompactor\ScriptCompactor.exe "%2Scripts\Trace.js" "%2Scripts\Compacted\Trace.js" "Copyright (c)2006-2007 Stephen M. McKamey. http://JsonFx.net"
 
 xcopy "%2Scripts\JsonML.js" "%2..\JsonML\JsonML.js" /Y /R
 xcopy "%2Scripts\Compacted\JsonML.js" "%2..\JsonML\JsonML_min.js" /Y /R
 
-%1FileConcat\FileConcat.exe "%2Scripts\JsonFx.js" "%2Scripts\Core.js" "%2Scripts\JSON.js" "%2Scripts\JsonML.js" "%2Scripts\IO.js" "%2Scripts\UI.js"
+%1FileConcat\FileConcat.exe "%2Scripts\JsonFx.js" "%2Scripts\Core.js" "%2Scripts\JSON.js" "%2Scripts\JsonML.js" "%2Scripts\IO.js" "%2Scripts\UI.js" "%2Scripts\UA.js"
 %1ScriptCompactor\ScriptCompactor.exe "%2Scripts\JsonFx.js" "%2Scripts\Compacted\JsonFx.js" "Copyright (c)2006-2007 Stephen M. McKamey. http://JsonFx.net" -F
 
 GOTO DONE
