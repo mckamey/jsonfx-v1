a47b56a63c13db3cc733f781fd21ccb1b38c3f52
Stephen <Stephen@8e687404-24dc-11de-a087-198e6c7179d9>
2007-03-01 11:26:31 +0000

    - slight tightening up and fixes with integration

--- trunk/External/UA.js
+++ trunk/External/UA.js
@@ -1,4 +1,5 @@
-﻿/*---------------------------------------------------------------------------*\
+﻿/*extern JsonFx */
+/*---------------------------------------------------------------------------*\
 	JsonFx.UI.setCssUserAgent
 
 	Copyright (c)2006-2007 Stephen M. McKamey
@@ -23,88 +24,86 @@ if ("undefined" === typeof JsonFx.UI) {
 	// variables and helper functions available via JavaScript closures
 	var PREFIX = " ua-";
 
-	/*string*/ function formatCss(/*string*/ browName, /*string*/ verStr) {
-		if (!browName) {
+	/*string*/ function formatCss(/*string*/ b, /*string*/ v) {
+		if (!b) {
 			return "";
 		}
 
-		while (browName.indexOf(' ') >=0 ) {
-			browName = browName.replace(' ', '-');
+		b = b.replace(/\s*/g, '-');
+		var vi = parseInt(v, 10);
+		var vf = parseFloat(v);
+		vf = (vf === vi && vf.toFixed) ?
+			vf.toFixed(1) : vf.toString();
+		vf = vf.replace(/\./g, '-');
+		if (!isFinite(vi)) {
+			return PREFIX+b;
 		}
-		var verInt = parseInt(verStr, 10);
-		var verFloat = parseFloat(verStr);
-		verFloat = (verFloat === verInt && verFloat.toFixed) ?
-			verFloat.toFixed(1) : verFloat.toString();
-		verFloat = verFloat.replace('.', '-');
-		if (isNaN(verInt)) {
-			return PREFIX+browName;
-		}
-		return PREFIX+browName+PREFIX+browName+verInt+PREFIX+browName+verFloat;
+		return PREFIX+b+PREFIX+b+vi+PREFIX+b+vf;
 	}
 
 	/*string*/ function buildCss() {
 		// Regex tested against (2006-06-11 @ 1600): http://en.wikipedia.org/wiki/User_agent
-		var Regex_MSHTML = new RegExp("(msie|microsoft internet explorer)[\\s/]*([0-9]+[\\.]?[0-9]*)");
-		var Regex_Gecko = new RegExp("rv[:]([0-9]+[\\.]?[0-9]*).*?gecko[/][0-9]+(\\s+(\\S+)[/]([0-9]+[\\.]?[0-9]*))?");
-		var Regex_AppleWebKit = new RegExp("applewebkit[/]([0-9]+[\\.]?[0-9]*).*?(\\S+)[/][v]?([0-9]+[\\.]?[0-9]*)");
-		var Regex_Opera = new RegExp("opera[\\s/]*([0-9]+[\\.]?[0-9]*)");
-		var Regex_MSPIE = new RegExp("(mspie|microsoft pocket internet explorer)[\\s/]*([0-9]+[\\.]?[0-9]*)");
-		var Regex_MozCompat = new RegExp("[(].*?(\\S+)[/]([0-9]+[\\.]?[0-9]*).*?[)]");
-		var Regex_Other = new RegExp("^([^/]+)[/]([0-9]+[\\.]?[0-9]*)");
-		var Regex_AOL = new RegExp("(america online browser|aol)[\\s/]*([0-9]+[\\.]?[0-9]*)");
+		var R_MSIE = new RegExp("(msie|microsoft internet explorer)[\\s/]*([0-9]+[\\.]?[0-9]*)");
+		var R_Gecko = new RegExp("rv[:]([0-9]+[\\.]?[0-9]*).*?gecko[/][0-9]+(\\s+(\\S+)[/]([0-9]+[\\.]?[0-9]*))?");
+		var R_AppleWebKit = new RegExp("applewebkit[/]([0-9]+[\\.]?[0-9]*).*?(\\S+)[/][v]?([0-9]+[\\.]?[0-9]*)");
+		var R_Opera = new RegExp("opera[\\s/]*([0-9]+[\\.]?[0-9]*)");
+		var R_MSPIE = new RegExp("(mspie|microsoft pocket internet explorer)[\\s/]*([0-9]+[\\.]?[0-9]*)");
+		var R_MozCompat = new RegExp("[(].*?(\\S+)[/]([0-9]+[\\.]?[0-9]*).*?[)]");
+		var R_Other = new RegExp("^([^/]+)[/]([0-9]+[\\.]?[0-9]*)");
+		var R_AOL = new RegExp("(america online browser|aol)[\\s/]*([0-9]+[\\.]?[0-9]*)");
 
 		var ua = navigator.userAgent.toLowerCase();
-		var bodyCss = PREFIX+navigator.platform.toLowerCase();
-		var browserName = null;
-		var browserVer = null;
+		var css = PREFIX+navigator.platform.toLowerCase();
+		var bName = null;// browser name
+		var bVer = null;// browser version
 
 		// aol uses multiple browsers so don't stop
-		if (Regex_AOL.exec(ua)) {
-			bodyCss += formatCss("aol", RegExp.$2);
+		if (R_AOL.exec(ua)) {
+			css += formatCss("aol", RegExp.$2);
 		}
 
 		// order is important as user-agents spoof each other	
-		if (Regex_Opera.exec(ua)) {
-			browserName = "opera";
-			browserVer = RegExp.$1;
-		} else if (Regex_MSHTML.exec(ua)) {
-			browserName = "msie";
-			browserVer = RegExp.$2;
-		} else if (Regex_MSPIE.exec(ua)) {
-			browserName = "mspie";
-			browserVer = RegExp.$2;
-		} else if (Regex_AppleWebKit.exec(ua)) {
-			browserName = "applewebkit";
-			browserVer = RegExp.$1;
+		if (R_Opera.exec(ua)) {
+			bName = "opera";
+			bVer = RegExp.$1;
+		} else if (R_MSIE.exec(ua)) {
+			bName = "msie";
+			bVer = RegExp.$2;
+		} else if (R_MSPIE.exec(ua)) {
+			bName = "mspie";
+			bVer = RegExp.$2;
+		} else if (R_AppleWebKit.exec(ua)) {
+			bName = "applewebkit";
+			bVer = RegExp.$1;
 
 			// also add AppleWebKit-brand version
-			bodyCss += formatCss(RegExp.$2, RegExp.$3);
-		} else if (Regex_Gecko.exec(ua)) {
-			browserName = "gecko";
-			browserVer = RegExp.$1;
+			css += formatCss(RegExp.$2, RegExp.$3);
+		} else if (R_Gecko.exec(ua)) {
+			bName = "gecko";
+			bVer = RegExp.$1;
 
 			// also add Gecko-brand version
-			bodyCss += formatCss(RegExp.$3, RegExp.$4);
-		} else if (Regex_MozCompat.exec(ua)) {
-			browserName = RegExp.$1;
-			browserVer = RegExp.$2;
-		} else if (Regex_Other.exec(ua)) {
-			browserName = RegExp.$1;
-			browserVer = RegExp.$2;
+			css += formatCss(RegExp.$3, RegExp.$4);
+		} else if (R_MozCompat.exec(ua)) {
+			bName = RegExp.$1;
+			bVer = RegExp.$2;
+		} else if (R_Other.exec(ua)) {
+			bName = RegExp.$1;
+			bVer = RegExp.$2;
 		}
 
-		// browserVer should hold parsed version string
-		if (browserVer) {
-			bodyCss += formatCss(browserName, browserVer);
+		// bVer should hold parsed version string
+		if (bVer) {
+			css += formatCss(bName, bVer);
 		}
 		
-		return bodyCss;
+		return css;
 	}
 
 	// calculate styles immediately, loop until can apply them
 	var uaCss = buildCss();
 
-	// using JavaScript closures to access the calculated bodyCss
+	// using JavaScript closures to access the calculated css
 	/*void*/ function appendCss() {
 
 		// assign user-agent classes
@@ -123,10 +122,9 @@ if ("undefined" === typeof JsonFx.UI) {
 
 		if (!document.body) {
 			setTimeout(appendCssLoop, 100);
-			return;
+		} else {
+			appendCss();
 		}
-
-		appendCss();
 	}
 
 	appendCssLoop();
