796813dcc97cd2694212f8e434fd65dd762c175f
Stephen <Stephen@8e687404-24dc-11de-a087-198e6c7179d9>
2007-03-18 14:32:53 +0000

    - added auto-versioning to assembly and scripts - update of script compactor to produce custom timestamps

--- trunk/JsonFx/JsonFx/Build.bat
+++ trunk/JsonFx/JsonFx/Build.bat
@@ -2,21 +2,26 @@
 If "%1" == "" GOTO HANDLE_ERROR
 If "%2" == "" GOTO HANDLE_ERROR
 
-%1ScriptCompactor\ScriptCompactor.exe "%2Scripts\JSON.js" "%2Scripts\Compacted\JSON.js" "http://json.org/js.html" -F
-%1ScriptCompactor\ScriptCompactor.exe "%2Scripts\JsonML.js" "%2Scripts\Compacted\JsonML.js" "http://JsonML.org"
-%1ScriptCompactor\ScriptCompactor.exe "%2Scripts\Core.js" "%2Scripts\Compacted\Core.js" "Copyright (c)2006-2007 Stephen M. McKamey. http://JsonFx.net"
-%1ScriptCompactor\ScriptCompactor.exe "%2Scripts\IO.js" "%2Scripts\Compacted\IO.js" "Copyright (c)2006-2007 Stephen M. McKamey. http://JsonFx.net"
-%1ScriptCompactor\ScriptCompactor.exe "%2Scripts\UI.js" "%2Scripts\Compacted\UI.js" "Copyright (c)2006-2007 Stephen M. McKamey. http://JsonFx.net"
-%1ScriptCompactor\ScriptCompactor.exe "%2Scripts\UA.js" "%2Scripts\Compacted\UA.js" "Copyright (c)2006-2007 Stephen M. McKamey. http://JsonFx.net"
-%1ScriptCompactor\ScriptCompactor.exe "%2Scripts\Utility.js" "%2Scripts\Compacted\Utility.js" "Copyright (c)2006-2007 Stephen M. McKamey. http://JsonFx.net"
-%1ScriptCompactor\ScriptCompactor.exe "%2Scripts\ServiceTest.js" "%2Scripts\Compacted\ServiceTest.js" "Copyright (c)2006-2007 Stephen M. McKamey. http://JsonFx.net"
-%1ScriptCompactor\ScriptCompactor.exe "%2Scripts\Trace.js" "%2Scripts\Compacted\Trace.js" "Copyright (c)2006-2007 Stephen M. McKamey. http://JsonFx.net"
+%1AutoVersioning\AutoVersioning.exe "%2Properties\AssemblyVersion.cs"
+
+set copyright="Copyright (c)2006-2007 http://JsonFx.net"
+set timestamp="'Version 1.0.'yyMM'.'ddHH"
+
+%1ScriptCompactor\ScriptCompactor.exe "%2Scripts\JSON.js" "%2Scripts\Compacted\JSON.js" "" "" -F
+%1ScriptCompactor\ScriptCompactor.exe "%2Scripts\JsonML.js" "%2Scripts\Compacted\JsonML.js" "" ""
+%1ScriptCompactor\ScriptCompactor.exe "%2Scripts\Core.js" "%2Scripts\Compacted\Core.js" %copyright% %timestamp%
+%1ScriptCompactor\ScriptCompactor.exe "%2Scripts\IO.js" "%2Scripts\Compacted\IO.js" %copyright% %timestamp%
+%1ScriptCompactor\ScriptCompactor.exe "%2Scripts\UI.js" "%2Scripts\Compacted\UI.js" %copyright% %timestamp%
+%1ScriptCompactor\ScriptCompactor.exe "%2Scripts\UA.js" "%2Scripts\Compacted\UA.js" %copyright% %timestamp%
+%1ScriptCompactor\ScriptCompactor.exe "%2Scripts\Utility.js" "%2Scripts\Compacted\Utility.js" %copyright% %timestamp%
+%1ScriptCompactor\ScriptCompactor.exe "%2Scripts\ServiceTest.js" "%2Scripts\Compacted\ServiceTest.js" %copyright% %timestamp%
+%1ScriptCompactor\ScriptCompactor.exe "%2Scripts\Trace.js" "%2Scripts\Compacted\Trace.js" %copyright% %timestamp%
 
 xcopy "%2Scripts\JsonML.js" "%2..\JsonML\JsonML.js" /Y /R
 xcopy "%2Scripts\Compacted\JsonML.js" "%2..\JsonML\JsonML_min.js" /Y /R
 
 %1FileConcat\FileConcat.exe "%2Scripts\JsonFx.js" "%2Scripts\Core.js" "%2Scripts\JSON.js" "%2Scripts\JsonML.js" "%2Scripts\UA.js" "%2Scripts\IO.js" "%2Scripts\UI.js"
-%1ScriptCompactor\ScriptCompactor.exe "%2Scripts\JsonFx.js" "%2Scripts\Compacted\JsonFx.js" "Copyright (c)2006-2007 Stephen M. McKamey. http://JsonFx.net" -F
+%1ScriptCompactor\ScriptCompactor.exe "%2Scripts\JsonFx.js" "%2Scripts\Compacted\JsonFx.js" %copyright% %timestamp% -F
 
 GOTO DONE
 
---
+++ trunk/JsonFx/JsonFx/Properties/AssemblyVersion.cs
@@ -0,0 +1,7 @@
+/*-------------------------------------------------------------*\
+    This is generated code.  All changes will be overwritten.
+\*-------------------------------------------------------------*/
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.0.0703.1808")]
