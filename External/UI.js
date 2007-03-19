1c12b34344471ed095195724ef4c06ede5321aa5
Stephen McKamey <mckamey@users.noreply.github.com>
2007-03-19 00:37:20 +0000

    - removed the need for setTimout in expando binding

--- External/UI.js
+++ External/UI.js
@@ -514,43 +514,43 @@ JsonFx.UI.History.onchange = null;
 		return true;
 	}
 };
-
 /*void*/ JsonFx.UI.expandoBind = function(/*element*/ elem) {
 	// call after elements have been added to document
-	window.setTimeout(
-		function() {
-			var target = document.getElementById(elem.getAttribute("for"));
+	var target = null;
+	try {
+		elem.style.cursor = "pointer";
+	} catch (ex) {
+		// IE 5.x
+		elem.style.cursor = "hand";
+	}
+	elem.className += " jsonfx-expanded";
+
+	elem.onclick = function (/*event*/ evt) {
+		if (!target) {
+			// using a closure so only lookup once
+			target = document.getElementById(elem.getAttribute("for"));
 			if (!target) {
 				target = elem.nextSibling;
 				while (target && !target.tagName) {
 					target = target.nextSibling;
 				}
 			}
-			if (target) {
-				try {
-					elem.style.cursor = "pointer";
-				} catch (ex) {
-					elem.style.cursor = "hand";
-				}
-				elem.className += " jsonfx-expanded";
+		}
 
-				elem.onclick = function (/*event*/ evt) {
-					if (JsonFx.UI.expando(elem, target)) {
-						elem.className = elem.className.replace(/\s*jsonfx-expanded/g, " jsonfx-collapsed");
-					} else {
-						elem.className = elem.className.replace(/\s*jsonfx-collapsed/g, " jsonfx-expanded");
-					}
-					return false;
-				};
-				elem.ondblclick = function (/*event*/ evt) {
-					if (elem.click) {
-						elem.click();
-					}
-					window.setTimeout( function(){JsonFx.UI.clearTextSelection();}, 0);
-					return false;
-				};
-			}
-		}, 0);
+		if (JsonFx.UI.expando(elem, target)) {
+			elem.className = elem.className.replace(/\s*jsonfx-expanded/g, " jsonfx-collapsed");
+		} else {
+			elem.className = elem.className.replace(/\s*jsonfx-collapsed/g, " jsonfx-expanded");
+		}
+		return false;
+	};
+	elem.ondblclick = function (/*event*/ evt) {
+		if (elem.click) {
+			elem.click();
+		}
+		window.setTimeout( function(){JsonFx.UI.clearTextSelection();}, 0);
+		return false;
+	};
 };
 /*void*/ JsonFx.UI.expandoUnbind = function(/*element*/ elem) {
 	if ("undefined" !== typeof elem.expando) {
