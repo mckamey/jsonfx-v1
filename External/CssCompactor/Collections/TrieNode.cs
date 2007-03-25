254febebd390b155c707e9bb58cab6ac8602c273
Stephen McKamey <mckamey@users.noreply.github.com>
2007-03-25 13:09:34 +0000

    - initial checkin - parses most of the time - 3 different filter implementations (will choose 1 when parsing bugs fixed)

---
+++ External/CssCompactor/Collections/TrieNode.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+
+namespace BuildTools.Collections
+{
+	/// <summary>
+	/// A generic node for building a Trie
+	/// </summary>
+	/// <typeparam name="TKey">the Type used for the node path</typeparam>
+	/// <typeparam name="TValue">the Type used for the node value</typeparam>
+	/// <remarks>
+	/// http://en.wikipedia.org/wiki/Trie
+	/// </remarks>
+	public class TrieNode<TKey, TValue>
+	{
+		#region Fields
+
+		private TValue value = default(TValue);
+		private readonly IDictionary<TKey, TrieNode<TKey, TValue>> children;
+
+		#endregion Fields
+
+		#region Init
+
+		/// <summary>
+		/// Ctor
+		/// </summary>
+		public TrieNode() : this(-1)
+		{
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="capacity"></param>
+		public TrieNode(int capacity)
+		{
+			if (capacity < 1)
+			{
+				this.children = new Dictionary<TKey, TrieNode<TKey, TValue>>();
+			}
+			else
+			{
+				this.children = new Dictionary<TKey, TrieNode<TKey, TValue>>(capacity);
+			}
+		}
+
+		#endregion Init
+
+		#region Properties
+
+		public TrieNode<TKey, TValue> this[TKey key]
+		{
+			get
+			{
+				if (!this.children.ContainsKey(key))
+				{
+					return null;
+				}
+				return this.children[key];
+			}
+			protected set { this.children[key] = value; }
+		}
+
+		public TValue Value
+		{
+			get { return this.value; }
+			protected set
+			{
+				if (!EqualityComparer<TValue>.Default.Equals(this.value, default(TValue)))
+				{
+					throw new InvalidOperationException("Trie path collision: the value for TrieNode<"+value.GetType().Name+"> has already been assigned.");
+				}
+				this.value = value;
+			}
+		}
+
+		public bool HasValue
+		{
+			get
+			{
+				return !EqualityComparer<TValue>.Default.Equals(this.value, default(TValue));
+			}
+		}
+
+		#endregion Properties
+
+		#region Methods
+
+		public bool Contains(TKey key)
+		{
+			return this.children.ContainsKey(key);
+		}
+
+		#endregion Methods
+	}
+}
---
+++ External/CssCompactor/IO/FilterTrie.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+
+using BuildTools.Collections;
+
+namespace BuildTools.IO
+{
+	/// <summary>
+	/// Defines a character sequence to filter out when reading.
+	/// </summary>
+	/// <remarks>
+	/// If the sequence exists in the read source, it will be read out as if it was never there.
+	/// </remarks>
+	public struct ReadFilter
+	{
+		#region Fields
+
+		public readonly string StartToken;
+		public readonly string EndToken;
+
+		#endregion Fields
+
+		#region Init
+
+		public ReadFilter(string start, string end)
+		{
+			if (String.IsNullOrEmpty(start))
+			{
+				throw new ArgumentNullException("start");
+			}
+			if (String.IsNullOrEmpty(end))
+			{
+				throw new ArgumentNullException("end");
+			}
+
+			this.StartToken = start;
+			this.EndToken = end;
+		}
+
+		#endregion Init
+	}
+
+	/// <summary>
+	/// Creates a Trie out of ReadFilters
+	/// </summary>
+	public class FilterTrie : TrieNode<char, string>
+	{
+		#region Constants
+
+		private const int DefaultTrieWidth = 1;
+
+		#endregion Constants
+
+		#region Init
+
+		public FilterTrie(IEnumerable<ReadFilter> filters) : base(DefaultTrieWidth)
+		{
+			// load trie
+			foreach (ReadFilter filter in filters)
+			{
+				TrieNode<char, string> node = this;
+
+				// build out the path for StartToken
+				foreach (char ch in filter.StartToken)
+				{
+					if (!node.Contains(ch))
+					{
+						node = node[ch] = new TrieNode<char, string>(DefaultTrieWidth);
+					}
+					else
+					{
+						node = node[ch];
+					}
+				}
+
+				// at the end of StartToken path is the EndToken
+				node.Value = filter.EndToken;
+			}
+		}
+
+		#endregion Init
+	}
+}
