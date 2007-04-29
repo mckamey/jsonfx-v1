e96a10c54261ec77272deb94fd13d83dbc9020fd
Stephen McKamey <mckamey@users.noreply.github.com>
2007-04-29 01:29:06 +0000

    - added BuildTools source, auto-gen version - strongly signed assembly

---
+++ External/BuildTools/Properties/AssemblyVersion.cs
@@ -0,0 +1,11 @@
+/*------------------------------------------------------------------*\
+	This is auto-generated code.  All changes will be overwritten.
+\*------------------------------------------------------------------*/
+
+#region
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.0.0704.2818")]
+
+#endregion
