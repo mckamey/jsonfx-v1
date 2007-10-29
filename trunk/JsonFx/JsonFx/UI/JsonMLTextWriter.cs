7d59fac770003a4635344069a118a68ea52f5951
Stephen <Stephen@8e687404-24dc-11de-a087-198e6c7179d9>
2007-10-29 15:16:20 +0000

    - moved all parsing out of JsonMLTextWriter - added placeholders for HtmlDistiller as the literal parser

--- trunk/JsonFx/JsonFx/UI/JsonMLTextWriter.cs
+++ trunk/JsonFx/JsonFx/UI/JsonMLTextWriter.cs
@@ -5,29 +5,33 @@ using System.Text.RegularExpressions;
 
 namespace JsonFx.UI
 {
-	public class JsonMLTextWriter : System.Web.UI.XhtmlTextWriter
+	/// <summary>
+	/// Adapter between HtmlTextWriter interface and JsonControlBuilder internals
+	/// </summary>
+	public class JsonMLTextWriter : XhtmlTextWriter
 	{
 		#region Fields
 
-		private const string Pattern_Comment = @"(<!DOCTYPE[^>]*>)|(<!--(.)*?-->)";
-		private const string Pattern_Tag = "\\G<(?<tagname>[\\w:\\.]+)(\\s+(?<attrname>\\w[-\\w:]*)(\\s*=\\s*\"(?<attrval>[^\"]*)\"|\\s*=\\s*'(?<attrval>[^']*)'|\\s*=\\s*(?<attrval><%#.*?%>)|\\s*=\\s*(?<attrval>[^\\s=/>]*)|(?<attrval>\\s*?)))*\\s*(?<empty>/)?>";
-		private const string Pattern_EndTag = @"\G</(?<tagname>[\w:\.]+)\s*>";
-
-		private JsonControlBuilder builder;
-		private static readonly Regex Regex_Tag = new Regex(Pattern_Tag, RegexOptions.Singleline|RegexOptions.Multiline|RegexOptions.Compiled);
-		private static readonly Regex Regex_EndTag = new Regex(Pattern_EndTag, RegexOptions.Singleline|RegexOptions.Multiline|RegexOptions.Compiled);
-		private static readonly Regex Regex_Comment = new Regex(Pattern_Comment, RegexOptions.Singleline|RegexOptions.Multiline|RegexOptions.IgnoreCase);
+		private readonly JsonControlBuilder builder;
 
 		#endregion Fields
 
 		#region Init
 
+		/// <summary>
+		/// Ctor
+		/// </summary>
+		/// <param name="writer"></param>
 		public JsonMLTextWriter(TextWriter writer) : base(new NullTextWriter())
 		{
 			this.builder = new JsonControlBuilder(writer);
-			this.builder.PreProcess = new PreProcessLiteral(this.ParseLiteral);
 		}
 
+		/// <summary>
+		/// Ctor
+		/// </summary>
+		/// <param name="writer"></param>
+		/// <param name="tabString"></param>
 		public JsonMLTextWriter(TextWriter writer, string tabString) : base(new NullTextWriter(), tabString)
 		{
 			this.builder = new JsonControlBuilder(writer);
@@ -39,56 +43,41 @@ namespace JsonFx.UI
 
 		public override void AddAttribute(string name, string value)
 		{
-			// calls AddAttribute(string name, string value, HtmlTextWriterAttribute key)
-			//base.AddAttribute(name, value);
-
 			this.builder.StoreAttribute(name, value);
 		}
 
 		public override void AddAttribute(string name, string value, bool fEncode)
 		{
-			//base.AddAttribute(name, value, fEncode);
-
 			this.builder.StoreAttribute(name, value);
 		}
 
 		protected override void AddAttribute(string name, string value, HtmlTextWriterAttribute key)
 		{
-			///base.AddAttribute(name, value, key);
 			this.builder.StoreAttribute(base.GetAttributeName(key), value);
 		}
 
 		public override void AddAttribute(HtmlTextWriterAttribute key, string value)
 		{
-			//base.AddAttribute(key, value);
 			this.builder.StoreAttribute(base.GetAttributeName(key), value);
 		}
 
 		public override void AddAttribute(HtmlTextWriterAttribute key, string value, bool fEncode)
 		{
-			//base.AddAttribute(key, value, fEncode);
 			this.builder.StoreAttribute(base.GetAttributeName(key), value);
 		}
 
 		public override void AddStyleAttribute(string name, string value)
 		{
-			// calls AddStyleAttribute(string name, string value, HtmlTextWriterStyle key)
-			//base.AddStyleAttribute(name, value);
-
 			this.builder.StoreStyle(name, value);
 		}
 
 		protected override void AddStyleAttribute(string name, string value, HtmlTextWriterStyle key)
 		{
-			//base.AddStyleAttribute(name, value, key);
 			this.builder.StoreStyle(base.GetStyleName(key), value);
 		}
 
 		public override void AddStyleAttribute(HtmlTextWriterStyle key, string value)
 		{
-			// calls AddStyleAttribute(string name, string value, HtmlTextWriterStyle key)
-			//base.AddStyleAttribute(key, value);
-
 			this.builder.StoreStyle(base.GetStyleName(key), value);
 		}
 
@@ -102,11 +91,6 @@ namespace JsonFx.UI
 			base.Close();
 			this.Dispose();
 		}
-
-		//public override System.Runtime.Remoting.ObjRef CreateObjRef(Type requestedType)
-		//{
-		//    return base.CreateObjRef(requestedType);
-		//}
 
 		protected override void Dispose(bool disposing)
 		{
@@ -124,7 +108,6 @@ namespace JsonFx.UI
 		{
 			// trap this, we don't want HtmlEncoded attributes
 
-			//return base.EncodeAttributeValue(attrKey, value);
 			return value;
 		}
 
@@ -224,10 +207,11 @@ namespace JsonFx.UI
 			return base.OnTagRender(name, key);
 		}
 
-		//protected override void OutputTabs()
-		//{
-		//    base.OutputTabs();
-		//}
+		protected override void OutputTabs()
+		{
+			// trap this as we don't need extra whitespace
+			//base.OutputTabs();
+		}
 
 		//protected override string RenderAfterContent()
 		//{
@@ -251,19 +235,16 @@ namespace JsonFx.UI
 
 		public override void RenderBeginTag(string tagName)
 		{
-			//base.RenderBeginTag(tagName);
 			this.builder.PushTag(tagName);
 		}
 
 		public override void RenderBeginTag(HtmlTextWriterTag tagKey)
 		{
-			//base.RenderBeginTag(tagKey);
 			this.builder.PushTag(base.GetTagName(tagKey));
 		}
 
 		public override void RenderEndTag()
 		{
-			//base.RenderEndTag();
 			this.builder.PopTag();
 		}
 
@@ -279,20 +260,16 @@ namespace JsonFx.UI
 
 		public override void Write(char value)
 		{
-#warning Need to remove this filter
-			if (value != '>')
-				this.Write("{0}", value);
+			this.Write("{0}", value);
 		}
 
 		public override void Write(char[] buffer)
 		{
-			//this.Write(buffer);
 			this.Write(new String(buffer));
 		}
 
 		public override void Write(char[] buffer, int index, int count)
 		{
-			//base.Write(buffer, index, count);
 			this.Write(new String(buffer, index, count));
 		}
 
@@ -328,38 +305,30 @@ namespace JsonFx.UI
 
 		public override void Write(string format, object arg0)
 		{
-			//base.Write(format, arg0);
 			this.builder.AddLiteral(String.Format(format, arg0));
 		}
 
 		public override void Write(string format, object arg0, object arg1)
 		{
-			//base.Write(format, arg0, arg1);
 			this.builder.AddLiteral(String.Format(format, arg0, arg1));
 		}
 
 		public override void Write(string format, object arg0, object arg1, object arg2)
 		{
-			//base.Write(format, arg0, arg1, arg2);
 			this.builder.AddLiteral(String.Format(format, arg0, arg1, arg2));
 		}
 
 		public override void Write(string format, params object[] arg)
 		{
-			//base.Write(format, arg);
 			this.builder.AddLiteral(String.Format(format, arg));
 		}
 
 		public override void Write(string value)
 		{
-			//base.Write(value);
-
-			if (String.IsNullOrEmpty(value))
+			if (!String.IsNullOrEmpty(value))
 			{
-				return;
+				this.builder.AddLiteral(value);
 			}
-
-			this.builder.AddLiteral(value);
 		}
 
 		public override void Write(uint value)
@@ -378,22 +347,21 @@ namespace JsonFx.UI
 		//    base.WriteAttribute(name, value);
 		//}
 
-		public override void WriteAttribute(string name, string value, bool fEncode)
+		public override void WriteAttribute(string name, string value, bool encode)
 		{
-			//base.WriteAttribute(name, value, fEncode);
 			this.builder.AddAttribute(name, value);
 		}
 
 		public override void WriteBeginTag(string tagName)
 		{
-			//base.WriteBeginTag(tagName);
 			this.builder.PushTag(tagName);
 		}
 
-		//public override void WriteBreak()
-		//{
-		//    base.WriteBreak();
-		//}
+		public override void WriteBreak()
+		{
+			// writes out <br /> tag
+			base.WriteBreak();
+		}
 
 		public override void WriteEncodedText(string text)
 		{
@@ -412,21 +380,20 @@ namespace JsonFx.UI
 
 		public override void WriteEndTag(string tagName)
 		{
-			//base.WriteEndTag(tagName);
 			this.builder.PopTag();
 		}
 
 		public override void WriteFullBeginTag(string tagName)
 		{
-			//base.WriteFullBeginTag(tagName);
 			this.builder.PushTag(tagName);
 			this.builder.PopTag();
 		}
 
-		//public override void WriteLine()
-		//{
-		//    base.WriteLine();
-		//}
+		public override void WriteLine()
+		{
+			// trap this as we don't need extra whitespace
+			//base.WriteLine();
+		}
 
 		public override void WriteLine(bool value)
 		{
@@ -435,9 +402,7 @@ namespace JsonFx.UI
 
 		public override void WriteLine(char value)
 		{
-#warning Need to remove this filter
-			if (value != '>')
-				this.Write("{0}", value);
+			this.Write("{0}", value);
 		}
 
 		public override void WriteLine(char[] buffer)
@@ -447,7 +412,6 @@ namespace JsonFx.UI
 
 		public override void WriteLine(char[] buffer, int index, int count)
 		{
-			//base.WriteLine(buffer, index, count);
 			this.Write(new String(buffer, index, count));
 		}
 
@@ -518,100 +482,15 @@ namespace JsonFx.UI
 
 		public override void WriteStyleAttribute(string name, string value)
 		{
-			// this calls WriteStyleAttribute(string name, string value, bool fEncode)
-			//base.WriteStyleAttribute(name, value);
+			// this calls WriteStyleAttribute(string name, string value, bool encode)
 			this.builder.AddStyle(name, value);
 		}
 
-		public override void WriteStyleAttribute(string name, string value, bool fEncode)
+		public override void WriteStyleAttribute(string name, string value, bool encode)
 		{
-			//base.WriteStyleAttribute(name, value, fEncode);
 			this.builder.AddStyle(name, value);
 		}
 
 		#endregion Methods
-
-		#region Callback
-
-		private void ParseLiteral(string value)
-		{
-			// Need to check for and parse for literal HTML here :(
-
-			// Handles this:
-			// <li><a class="AsyncLink" href="
-			// <%=this.ResolveUrl("~/FAQ/") %>
-			// ">FAQ</a></li>
-
-			// filter comments and DOCTYPEs
-			value = Regex_Comment.Replace(value, "");
-
-			int start = 0;
-			int end = value.IndexOf('<');
-			while (end >= 0)
-			{
-				if (end-start > 0)
-				{
-					this.builder.AddLiteral(value.Substring(start, end-start));
-				}
-
-				// check if wasn't part of a tag
-				start = value.IndexOf('>', end);
-				if (start < 0)
-				{
-					start = end;
-					break;
-				}
-				else
-				{
-					start++;
-				}
-
-				Match match = Regex_Tag.Match(value, end, start-end);
-				if (match.Success)
-				{
-					string tagName = match.Groups["tagname"].Value;
-
-					// open tag
-					this.WriteBeginTag(tagName);
-
-					// write out attributes
-					int attribCount = Math.Min(
-						match.Groups["attrname"].Captures.Count,
-						match.Groups["attrval"].Captures.Count);
-					for (int i=0; i<attribCount; i++)
-					{
-						this.WriteAttribute(
-							match.Groups["attrname"].Captures[i].Value,
-							match.Groups["attrval"].Captures[i].Value);
-					}
-
-					if (!String.IsNullOrEmpty(match.Groups["empty"].Value))
-					{
-						// empty tag so immediately close it
-						this.WriteEndTag(tagName);
-					}
-				}
-				else
-				{
-					match = Regex_EndTag.Match(value, end, start-end);
-					if (match.Success)
-					{
-						// found a closing tag
-						this.WriteEndTag(match.Groups["tagname"].Value);
-					}
-					else
-					{
-						// wasn't part of a valid tag, but output as text
-						this.builder.AddLiteral(value.Substring(end, start-end));
-					}
-				}
-
-				end = value.IndexOf('<', start);
-			}
-
-			this.builder.AddLiteral(value.Substring(start));
-		}
-
-		#endregion Callback
 	}
 }
