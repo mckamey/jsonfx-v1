17038109f0f7cb98458078b14ede46bc5af969cb
Stephen <Stephen@8e687404-24dc-11de-a087-198e6c7179d9>
2008-05-29 05:32:14 +0000

    - moving JsonFxScriptHandler into its own file

--- trunk/JsonFx/JsonFx/Handlers/JsonFxScriptHandler.cs
+++ trunk/JsonFx/JsonFx/Handlers/JsonFxScriptHandler.cs
@@ -5,33 +5,6 @@ using System.Web;
 
 namespace JsonFx.Handlers
 {
-	public class ClientScriptHandler : ResourceHandler
-	{
-		#region Constants
-
-		// full source scripts: good for dev and debugging
-		internal const string ScriptPath = "JsonFx.Scripts.";
-
-		// compacted scripts: better for distribution and speed
-		internal const string CompactedScriptPath = "JsonFx.Scripts.Compacted.";
-
-		#endregion Constants
-
-		#region Properties
-
-		protected override string ResourceContentType
-		{
-			get { return "application/javascript"; }
-		}
-
-		protected override string  ResourceExtension
-		{
-			get { return ".js"; }
-		}
-
-		#endregion Properties
-	}
-
 	public class JsonFxScriptHandler : ClientScriptHandler
 	{
 		#region Handler Members
