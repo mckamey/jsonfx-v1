c15b3fad67b409d1f23d8de8b0c3c6c00b509f7e
Stephen <Stephen@8e687404-24dc-11de-a087-198e6c7179d9>
2008-05-29 02:37:57 +0000

    - updated to latest JSLint (2008-05-27)

--- trunk/External/BuildTools/Properties/AssemblyVersion.cs
+++ trunk/External/BuildTools/Properties/AssemblyVersion.cs
@@ -2,10 +2,10 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.0805.2818
+#region 1.0.0805.2819
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0805.2818")]
+[assembly: AssemblyVersion("1.0.0805.2819")]
 
 #endregion
