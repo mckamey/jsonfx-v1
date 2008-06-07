fded69d199a2070c1b796f4e142435d2c6add026
Stephen <Stephen@8e687404-24dc-11de-a087-198e6c7179d9>
2008-06-07 19:14:15 +0000

    - simplifying code paths

--- trunk/JsonFx/JsonFx.History/Scripts/JsonFx.History.js
+++ trunk/JsonFx/JsonFx.History/Scripts/JsonFx.History.js
@@ -4,7 +4,7 @@
 
 	Copyright (c)2006-2008 Stephen M. McKamey
 	Created: 2006-11-11-1759
-	Modified: 2008-06-07-0827
+	Modified: 2008-06-07-1202
 
 	Distributed under the terms of an MIT-style license:
 	http://jsonfx.net/BuildTools/License.txt
@@ -50,93 +50,54 @@ if ("undefined" === typeof JsonFx.UI) {
 
 JsonFx.History = {
 	/*object*/ h: null,
-	/*string*/ flag: "*",
 
-	init: function(/*DOM*/ elem, /*function*/ callback, /*bool*/ mode) {
+	load: function(/*DOM*/ elem, /*function*/ callback, /*bool*/ start, /*string*/ url) {
 
-		if (JsonFx.History.h) {
-			// IE doesn't let us change the original onload
-			JsonFx.History.changed();
-			return;
-		}
-
-		// store the history for easy lookup, first init wins
-		JsonFx.History.h =
-			{
-				elem: elem,
-				callback: callback,
-				virtual: !mode
-			};
-
-		if (elem.onload) {
-			// change the onload to onchange
-			elem.onload = JsonFx.History.changed;
-		}
-
-		// check if a history exists
-		JsonFx.History.ensureState();
-	},
-
-	/*void*/ ensureState: function() {
-
-		var h = JsonFx.History.h;
-		if (!h || !h.elem || !h.callback) {
+		if (!elem || "function" !== typeof callback) {
 			return;
 		}
 
 		var info;
-		if (JsonFx.History.isFlagged()) {
-			// reloaded page
-			info = JsonFx.History.getState();
-			if (info) {
-				h.callback(info);
-			}
-		} else if (!h.elem.onload) {
-			// IE needs a little help ensuring that
-			// initial state is stored in history
-			var callback = JsonFx.History.h.callback;
-			JsonFx.History.h.callback = null;
+		if (!JsonFx.History.h) {
+			// initialization
 
-			// IE doesn't store the first state, so we re-save
-			info = JsonFx.History.getState();
-			if (info) {
-				JsonFx.History.save(info);
-			}
+			JsonFx.History.h =
+				{
+					elem: elem,
+					callback: callback,
+					url: url
+				};
 
-			// reconnect callback
-			JsonFx.History.h.callback = callback;
+			info = JsonFx.History.getState(JsonFx.History.h);
+			if (info) {
+				// previously cached page was reloaded
+				callback(info);
+
+			} else if (!elem.onload && start) {
+				// IE needs a little help ensuring that
+				// initial state is stored in history
+				JsonFx.History.h.callback = null;
+
+				// re-save start state
+				JsonFx.History.save(start);
+				// reconnect callback
+				JsonFx.History.h.callback = callback;
+			}
+			return;
+
+		} else {
+			// onchange
+
+			info = JsonFx.History.getState(JsonFx.History.h) || start;
+			if (info) {
+				callback(info);
+			}
 		}
 	},
 
-	/*bool*/ isFlagged: function() {
+	/*object*/ getState: function(/*object*/ h) {
 
-		var h = JsonFx.History.h;
-		if (!h || !h.elem) {
-			return false;
-		}
-
-		var doc = JsonFx.UI.getIFrameDocument(h.elem);
-		if (!doc || !doc.location || !doc.body) {
-			return false;
-		}
-
-		var info = h.virtual ?
-			doc.body.innerHTML :
-			doc.location.search;
-
-		if (info && !h.virtual) {
-			// strip query char and decode
-			info = info.substr(1);
-			info = decodeURIComponent(info);
-		}
-
-		return info && (info.charAt(0) === JsonFx.History.flag);
-	},
-
-	/*object*/ getState: function() {
-
-		var h = JsonFx.History.h;
-		if (!h || !h.elem) {
+		if (!h) {
 			return null;
 		}
 
@@ -145,37 +106,20 @@ JsonFx.History = {
 			return null;
 		}
 
-		var info = h.virtual ?
-			doc.body.innerHTML :
-			doc.location.search;
+		var info = h.url ?
+			doc.location.search :
+			doc.body.innerHTML;
 
-		if (info && !h.virtual) {
+		if (info && h.url) {
 			// strip query char and decode
 			info = info.substr(1);
 			info = decodeURIComponent(info);
-		}
-		if (info && info.charAt(0) === JsonFx.History.flag) {
-			// strip cache flag
-			info = info.substr(1);
 		}
 		if (!info) {
 			return null;
 		}
 
 		return JSON.parse(info);
-	},
-
-	/*void*/ changed: function() {
-
-		var h = JsonFx.History.h;
-		if (!h || "function" !== typeof h.callback) {
-			return;
-		}
-
-		var info = JsonFx.History.getState();
-		if (info) {
-			h.callback(info);
-		}
 	},
 
 	/*void*/ save: function(/*object*/ info) {
@@ -185,28 +129,19 @@ JsonFx.History = {
 			return;
 		}
 
-		if (!h.elem) {
-			// doesn't support history or no binding
-			if ("function" === typeof h.callback) {
-				h.callback(info);
-			}
-			return;
-		}
-
 		var doc = JsonFx.UI.getIFrameDocument(h.elem);
 		if (!doc || !doc.location || !doc.write) {
-			// Opera 8 doesn't trigger onload so no history
-			h.elem = null;
+			// error just call method directly
 			if ("function" === typeof h.callback) {
 				h.callback(info);
 			}
 			return;
 		}
 
-		info = JsonFx.History.flag+JSON.stringify(info);
-		if (!h.virtual) {
+		info = JSON.stringify(info);
+		if (h.url) {
 			// encode the serialized object into the query string
-			doc.location.search = '?'+encodeURIComponent(info);
+			doc.location.href = h.url+'?'+encodeURIComponent(info);
 		} else {
 			// create a new document containing the serialized object
 			doc.open();
