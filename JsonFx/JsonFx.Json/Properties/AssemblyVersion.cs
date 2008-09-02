ad905ca83ec8a66957ed1977039c72b48ab778e5
Stephen McKamey <mckamey@users.noreply.github.com>
2008-09-02 00:30:13 +0000

    - added static methods for fast de/serialization

--- JsonFx/JsonFx.Json/Properties/AssemblyVersion.cs
+++ JsonFx/JsonFx.Json/Properties/AssemblyVersion.cs
@@ -2,10 +2,10 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.0808.1613
+#region 1.0.0809.0111
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0808.1613")]
+[assembly: AssemblyVersion("1.0.0809.0111")]
 
 #endregion
