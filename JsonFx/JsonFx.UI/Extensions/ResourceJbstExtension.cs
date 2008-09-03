c33058131ea74d62f1c5f1f8b68f688f58decfb6
Stephen McKamey <mckamey@users.noreply.github.com>
2008-09-03 16:01:20 +0000

    - added declarative JbstExtension - implemented resource string support as JbstExtension

---
+++ JsonFx/JsonFx.UI/Extensions/ResourceJbstExtension.cs
@@ -0,0 +1,31 @@
+using System;
+
+using JsonFx.Json;
+
+namespace JsonFx.Extensions
+{
+	public class ResourceJbstExtension : JbstExtension
+	{
+		#region Constants
+
+		private const string ResourceLookupFormat =
+			@"function() {{
+				return (JsonFx.ResX && JsonFx.ResX[{0}]) || {1};
+			}}";
+
+		#endregion Constants
+
+		#region JbstExtension Members
+
+		protected override string Eval(string value)
+		{
+
+			return String.Format(
+				ResourceLookupFormat,
+				JsonWriter.Serialize(value),
+				JsonWriter.Serialize("$$"+value+"$$"));
+		}
+
+		#endregion JbstExtension Members
+	}
+}
