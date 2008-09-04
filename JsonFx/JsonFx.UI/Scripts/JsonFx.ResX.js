f8d1d01cdc62d3e905c1ccbd0a0e5a55037e6415
Stephen McKamey <mckamey@users.noreply.github.com>
2008-09-04 16:04:07 +0000

    - adding start of client-side resx

---
+++ JsonFx/JsonFx.UI/Scripts/JsonFx.ResX.js
@@ -0,0 +1,42 @@
+/*global JsonFx */
+/*---------------------------------------------------------------------------*\
+	JsonFx.ResX
+
+	Copyright (c)2008 Stephen M. McKamey
+	Created: 2008-09-04-0845
+	Modified: 2008-09-04-0845
+\*---------------------------------------------------------------------------*/
+
+/* namespace JsonFx.ResX */
+if ("undefined" === typeof JsonFx) {
+	window.JsonFx = {};
+}
+
+if ("undefined" === typeof JsonFx.ResX) {
+
+	JsonFx.ResX = (function(){
+		// create private member via closure
+		var resx = {};
+
+		/*void*/ this.add = function(/*object*/ r) {
+			if (!r) {
+				return;
+			}
+
+			// merge in the new values
+			for (var k in r) {
+				if (r.hasOwnProperty(k)) {
+					resx[k] = r[k];
+				}
+			}
+		};
+
+		/*object*/ this.get = function(/*string*/ k, /*object*/ d) {
+			if ("string" !== typeof k) {
+				return d;
+			}
+
+			return resx.hasOwnProperty(k) ? resx[k] : d;
+		};
+	})();
+}
