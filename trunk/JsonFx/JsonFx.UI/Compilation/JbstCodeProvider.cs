2ce7cdff0e1dcc3b30243ca0414200a0621be437
Stephen <Stephen@8e687404-24dc-11de-a087-198e6c7179d9>
2008-09-05 15:53:14 +0000

    - added code generation for populating the list of GlobalizationKeys

--- trunk/JsonFx/JsonFx.UI/Compilation/JbstCodeProvider.cs
+++ trunk/JsonFx/JsonFx.UI/Compilation/JbstCodeProvider.cs
@@ -70,6 +70,40 @@ namespace JsonFx.Compilation
 
 		protected override void GenerateCodeExtensions(CodeTypeDeclaration resourceType)
 		{
+			#region private static readonly string[] g11nKeys
+
+			CodeMemberField field = new CodeMemberField();
+			field.Name = "g11nKeys";
+			field.Type = new CodeTypeReference(typeof(string[]));
+			field.Attributes = MemberAttributes.Private|MemberAttributes.Static|MemberAttributes.Final;
+
+			CodeArrayCreateExpression arrayInit = new CodeArrayCreateExpression(field.Type, this.g11nKeys.Count);
+			foreach (string key in this.g11nKeys)
+			{
+				arrayInit.Initializers.Add(new CodePrimitiveExpression(key));
+			}
+			field.InitExpression = arrayInit;
+
+			resourceType.Members.Add(field);
+
+			#endregion private static readonly string[] g11nKeys
+
+			#region public override string[] GlobalizationKeys { get; }
+
+			// add a readonly property returning the static data
+			CodeMemberProperty property = new CodeMemberProperty();
+			property.Name = "GlobalizationKeys";
+			property.Type = field.Type;
+			property.Attributes = MemberAttributes.Public|MemberAttributes.Override;
+			property.HasGet = true;
+			// get { return g11nKeys; }
+			property.GetStatements.Add(new CodeMethodReturnStatement(
+				new CodeFieldReferenceExpression(
+					new CodeTypeReferenceExpression(resourceType.Name),
+					field.Name)));
+			resourceType.Members.Add(property);
+
+			#endregion public override string[] GlobalizationKeys { get; }
 		}
 
 		protected override void ProcessResource(
