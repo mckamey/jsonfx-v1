f0cd23d3f12d0ccba3846379f1e6481e0c519ff6
Stephen <Stephen@8e687404-24dc-11de-a087-198e6c7179d9>
2008-09-09 03:16:52 +0000

    - adding strong signing to JsonFx

--- trunk/JsonFx/JsonFx.UI/Properties/AssemblyInfo.cs
+++ trunk/JsonFx/JsonFx.UI/Properties/AssemblyInfo.cs
@@ -32,27 +32,19 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: ObfuscateAssembly(false, StripAfterObfuscation=false)]
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyTitle("JsonFx.UI")]
-[assembly: AssemblyDescription("JsonFx.NET UI Support")]
+[assembly: AssemblyDescription("JsonFx.NET UI")]
 [assembly: AssemblyProduct("JsonFx.NET")]
 [assembly: AssemblyCopyright("Copyright © 2006-2008 Stephen M. McKamey. All rights reserved.")]
 [assembly: AssemblyCompany("http://jsonfx.net")]
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
+#elif STRONG
+[assembly: AssemblyConfiguration("Signed")]
 #else
 [assembly: AssemblyConfiguration("Release")]
 #endif
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2A9F808E-C569-4713-BD17-998100B868BA")]
-
+[assembly: ObfuscateAssembly(false, StripAfterObfuscation=false)]
