a79dac6963055c7db21927866f485b4d8b23bb83
Stephen McKamey <mckamey@users.noreply.github.com>
2008-09-13 19:10:49 +0000

    - ensuring build result dependencies

--- JsonFx/JsonFx.Client/Compilation/ResourceBuildProvider.cs
+++ JsonFx/JsonFx.Client/Compilation/ResourceBuildProvider.cs
@@ -212,14 +212,32 @@ namespace JsonFx.Compilation
 
 			#endregion public sealed class ResourceTypeName
 
-			#region private static readonly byte[] GzippedBytes
+			#region private static readonly string[] vpathDependencies
 
 			CodeMemberField field = new CodeMemberField();
+			field.Name = "vpathDependencies";
+			field.Type = new CodeTypeReference(typeof(string[]));
+			field.Attributes = MemberAttributes.Private|MemberAttributes.Static|MemberAttributes.Final;
+
+			CodeArrayCreateExpression arrayInit = new CodeArrayCreateExpression(field.Type, this.VirtualPathDependencies.Count);
+			foreach (string key in this.VirtualPathDependencies)
+			{
+				arrayInit.Initializers.Add(new CodePrimitiveExpression(key));
+			}
+			field.InitExpression = arrayInit;
+
+			resourceType.Members.Add(field);
+
+			#endregion private static readonly string[] vpathDependencies
+
+			#region private static readonly byte[] GzippedBytes
+
+			field = new CodeMemberField();
 			field.Name = "GzippedBytes";
 			field.Type = new CodeTypeReference(typeof(byte[]));
 			field.Attributes = MemberAttributes.Private|MemberAttributes.Static|MemberAttributes.Final;
 
-			CodeArrayCreateExpression arrayInit = new CodeArrayCreateExpression(field.Type, gzippedBytes.Length);
+			arrayInit = new CodeArrayCreateExpression(field.Type, gzippedBytes.Length);
 			foreach (byte b in gzippedBytes)
 			{
 				arrayInit.Initializers.Add(new CodePrimitiveExpression(b));
@@ -248,10 +266,27 @@ namespace JsonFx.Compilation
 
 			#endregion private static readonly byte[] DeflatedBytes;
 
+			#region protected override string[] VirtualPathDependencies { get; }
+
+			// add a readonly property returning the static data
+			CodeMemberProperty property = new CodeMemberProperty();
+			property.Name = "VirtualPathDependencies";
+			property.Type = new CodeTypeReference(typeof(string[]));
+			property.Attributes = MemberAttributes.Family|MemberAttributes.Override;
+			property.HasGet = true;
+			// get { return vpathDependencies; }
+			property.GetStatements.Add(new CodeMethodReturnStatement(
+				new CodeFieldReferenceExpression(
+					new CodeTypeReferenceExpression(resourceType.Name),
+					"vpathDependencies")));
+			resourceType.Members.Add(property);
+
+			#endregion protected override string[] VirtualPathDependencies { get; }
+
 			#region public override string PrettyPrinted { get; }
 
 			// add a readonly property with the resource data
-			CodeMemberProperty property = new CodeMemberProperty();
+			property = new CodeMemberProperty();
 			property.Name = "PrettyPrinted";
 			property.Type = new CodeTypeReference(typeof(String));
 			property.Attributes = MemberAttributes.Public|MemberAttributes.Override;
@@ -434,7 +469,11 @@ namespace JsonFx.Compilation
 				this.pathDependencies.Add(base.VirtualPath);
 			}
 
-			this.pathDependencies.Add(virtualPath);
+			// attempt to dedup
+			if (!this.pathDependencies.Contains(virtualPath))
+			{
+				this.pathDependencies.Add(virtualPath);
+			}
 		}
 
 		void IResourceBuildHelper.AddAssemblyDependency(Assembly assembly)
@@ -448,7 +487,11 @@ namespace JsonFx.Compilation
 				}
 			}
 
-			this.assemblyDependencies.Add(assembly);
+			// attempt to dedup
+			if (!this.assemblyDependencies.Contains(assembly))
+			{
+				this.assemblyDependencies.Add(assembly);
+			}
 		}
 
 		TextReader IResourceBuildHelper.OpenReader(string virtualPath)
