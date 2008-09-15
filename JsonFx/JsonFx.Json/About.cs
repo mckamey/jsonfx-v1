144d54a3d8f84b36c9e7ae77f56af53c870fd27d
Stephen McKamey <mckamey@users.noreply.github.com>
2008-09-15 05:45:39 +0000

    - making about non-static and adding a static instance

--- JsonFx/JsonFx.Json/About.cs
+++ JsonFx/JsonFx.Json/About.cs
@@ -33,46 +33,61 @@ using System.Reflection;
 
 namespace JsonFx
 {
-	public static class About
+	/// <summary>
+	/// JsonFx metadata
+	/// </summary>
+	public sealed class About
 	{
 		#region Fields
 
-		public static readonly Version Version;
-		public static readonly string FullName;
-		public static readonly string Name;
-		public static readonly string Configuration;
-		public static readonly string Copyright;
-		public static readonly string Title;
-		public static readonly string Description;
-		public static readonly string Company;
+		public static readonly About Fx;
+
+		public readonly Version Version;
+		public readonly string FullName;
+		public readonly string Name;
+		public readonly string Configuration;
+		public readonly string Copyright;
+		public readonly string Title;
+		public readonly string Description;
+		public readonly string Company;
 
 		#endregion Fields
 
 		#region Init
 
+		/// <summary>
+		/// CCtor
+		/// </summary>
 		static About()
 		{
-			Assembly assembly = Assembly.GetAssembly(typeof(About));
+			About.Fx = new About(typeof(About).Assembly);
+		}
+
+		/// <summary>
+		/// Ctor
+		/// </summary>
+		public About(Assembly assembly)
+		{
 			AssemblyName name = assembly.GetName();
 
-			About.FullName = assembly.FullName;
-			About.Version = name.Version;
-			About.Name = name.Name;
+			this.FullName = assembly.FullName;
+			this.Version = name.Version;
+			this.Name = name.Name;
 
 			AssemblyCopyrightAttribute copyright = Attribute.GetCustomAttribute(assembly, typeof(AssemblyCopyrightAttribute)) as AssemblyCopyrightAttribute;
-			About.Copyright = (copyright != null) ? copyright.Copyright : String.Empty;
+			this.Copyright = (copyright != null) ? copyright.Copyright : String.Empty;
 
 			AssemblyDescriptionAttribute description = Attribute.GetCustomAttribute(assembly, typeof(AssemblyDescriptionAttribute)) as AssemblyDescriptionAttribute;
-			About.Description = (description != null) ? description.Description : String.Empty;
+			this.Description = (description != null) ? description.Description : String.Empty;
 
 			AssemblyTitleAttribute title = Attribute.GetCustomAttribute(assembly, typeof(AssemblyTitleAttribute)) as AssemblyTitleAttribute;
-			About.Title = (title != null) ? title.Title : String.Empty;
+			this.Title = (title != null) ? title.Title : String.Empty;
 
 			AssemblyCompanyAttribute company = Attribute.GetCustomAttribute(assembly, typeof(AssemblyCompanyAttribute)) as AssemblyCompanyAttribute;
-			About.Company = (company != null) ? company.Company : String.Empty;
+			this.Company = (company != null) ? company.Company : String.Empty;
 
 			AssemblyConfigurationAttribute config = Attribute.GetCustomAttribute(assembly, typeof(AssemblyConfigurationAttribute)) as AssemblyConfigurationAttribute;
-			About.Configuration = (config != null) ? config.Configuration : String.Empty;
+			this.Configuration = (config != null) ? config.Configuration : String.Empty;
 		}
 
 		#endregion Init
