d504be3b69ac7a532b12be0ae217f9aa76657f92
Stephen McKamey <mckamey@users.noreply.github.com>
2008-09-23 16:21:34 +0000

    - added example JBST

--- JsonFx/JsonFx.Template/Scripts/MyScript.js
+++ JsonFx/JsonFx.Template/Scripts/MyScript.js
@@ -1,6 +1,42 @@
-﻿/*global JsonFx */
+﻿/*global JsonFx, JsonML, Example */
 
 /* enable valid CSS to target browsers without reverting to CSS hacks */
 JsonFx.UI.setCssUserAgent();
 
-// Put your JavaScript here
+/*
+	it is a best practice to not clutter the global namespace
+	creating top level objects which contain variables and functions
+	allows us to simulate namespaces
+*/
+
+/* namespace Example */
+if ("undefined" === typeof window.Example) {
+	window.Example = {};
+}
+
+/*void*/ Example.initDisplay = function(/*DOM*/ elem) {
+	// some example data
+	var data = { "hostName" : window.location.host };
+
+	// this databinds the data to the template
+	var jbst = Example.congats.dataBind( data );
+
+	// this hydrates the resulting markup allowing dynamic behaviors to be bound to elements
+	jbst = JsonML.parse(jbst, JsonFx.Bindings.bindOne);
+
+	// clear the container contents
+	JsonFx.DOM.clear(elem);
+
+	// add the result to the container
+	if (elem && jbst) {
+		elem.appendChild(jbst);
+	}
+};
+
+/* setup test */
+JsonFx.Bindings.register(
+	"div",
+	"js-MessageArea",
+	Example.initDisplay,
+	null);
+
