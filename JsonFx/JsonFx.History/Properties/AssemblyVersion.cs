4f3a080b33d144720d272a34f355d8fc63655d21
Stephen McKamey <mckamey@users.noreply.github.com>
2008-10-26 23:41:25 +0000

    - updating to build v1.0.0810.2617

--- JsonFx/JsonFx.History/Properties/AssemblyVersion.cs
+++ JsonFx/JsonFx.History/Properties/AssemblyVersion.cs
@@ -2,10 +2,10 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.0810.2611
+#region 1.0.0810.2617
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0810.2611")]
+[assembly: AssemblyVersion("1.0.0810.2617")]
 
 #endregion
