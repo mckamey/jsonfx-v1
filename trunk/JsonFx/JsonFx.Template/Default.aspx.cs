53cf1b81b148442b02238302383cd6d08700fdad
Stephen <Stephen@8e687404-24dc-11de-a087-198e6c7179d9>
2008-10-31 05:40:02 +0000

    - replacing #if DEBUG with HttpContext.IsDebuggingEnabled

--- trunk/JsonFx/JsonFx.Template/Default.aspx.cs
+++ trunk/JsonFx/JsonFx.Template/Default.aspx.cs
@@ -4,11 +4,7 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-#if DEBUG
-		// Pretty-Print rendering helps when debugging through script
-		this.StyleImport.IsDebug = true;
-		this.ScriptInclude.IsDebug = true;
-#else
+#if !DEBUG
 		// improve the Yslow rating
 		JsonFx.Handlers.CompiledBuildResult.EnableStreamCompression(this.Context);
 #endif
