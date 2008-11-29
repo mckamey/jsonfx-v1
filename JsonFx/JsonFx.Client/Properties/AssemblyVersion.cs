46a627aae5d35477a775f441e8c473627df98f28
Stephen McKamey <mckamey@users.noreply.github.com>
2008-11-29 18:21:27 +0000

    - updating version stamp for release

--- JsonFx/JsonFx.Client/Properties/AssemblyVersion.cs
+++ JsonFx/JsonFx.Client/Properties/AssemblyVersion.cs
@@ -2,10 +2,10 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.811.2821
+#region 1.0.811.2910
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.811.2821")]
+[assembly: AssemblyVersion("1.0.811.2910")]
 
-#endregion 1.0.811.2821
+#endregion 1.0.811.2910
