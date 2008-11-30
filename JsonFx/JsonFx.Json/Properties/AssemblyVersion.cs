067b4f756e0fc173960a7242b5e5a831516e3c9f
Stephen McKamey <mckamey@users.noreply.github.com>
2008-11-30 22:37:07 +0000

    - updating version number

--- JsonFx/JsonFx.Json/Properties/AssemblyVersion.cs
+++ JsonFx/JsonFx.Json/Properties/AssemblyVersion.cs
@@ -2,10 +2,10 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.811.2910
+#region 1.0.812.114
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.811.2910")]
+[assembly: AssemblyVersion("1.0.812.114")]
 
-#endregion 1.0.811.2910
+#endregion 1.0.812.114
