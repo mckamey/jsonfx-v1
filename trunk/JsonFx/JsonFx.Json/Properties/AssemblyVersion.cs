0622c2883f97a09b0440018cd0f29bec0b4ef035
jsonfx@jsonfx.net <jsonfx@jsonfx.net@8e687404-24dc-11de-a087-198e6c7179d9>
2008-12-05 16:32:14 +0000

    - adding maximum depth check

--- trunk/JsonFx/JsonFx.Json/Properties/AssemblyVersion.cs
+++ trunk/JsonFx/JsonFx.Json/Properties/AssemblyVersion.cs
@@ -2,10 +2,10 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.812.116
+#region 1.0.812.509
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.812.116")]
+[assembly: AssemblyVersion("1.0.812.509")]
 
-#endregion 1.0.812.116
+#endregion 1.0.812.509
