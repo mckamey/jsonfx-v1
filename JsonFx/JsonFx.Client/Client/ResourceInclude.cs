425fa9955fcf341650c221a232f4c1c5f73fcf6c
Stephen McKamey <mckamey@users.noreply.github.com>
2009-01-19 00:15:40 +0000

    - enabling alternate renderings for stylesheets (links, custom attributes)

--- JsonFx/JsonFx.Client/Client/ResourceInclude.cs
+++ JsonFx/JsonFx.Client/Client/ResourceInclude.cs
@@ -1,25 +1,34 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Web;
 using System.Web.UI;
 using System.Globalization;
 using System.Threading;
+using System.Text;
 
 using JsonFx.Handlers;
 using JsonFx.Compilation;
 
 namespace JsonFx.Client
 {
+	public enum StyleIncludeType
+	{
+		Link,
+		Import
+	}
+
 	/// <summary>
 	/// Base control for referencing a ResourceHandler
 	/// </summary>
 	[ToolboxData("<{0}:ResourceInclude runat=\"server\"></{0}:ResourceInclude>")]
-	public class ResourceInclude : Control
+	public class ResourceInclude : Control, IAttributeAccessor
 	{
 		#region Constants
 
-		private const string StyleImport = "<style type=\"{0}\">@import url({1});</style>";
-		private const string ScriptInclude = "<script type=\"{0}\" src=\"{1}\"></script>";
+		private const string StyleLink = "<link type=\"{0}\" href=\"{1}\"{2} />";
+		private const string StyleImport = "<style type=\"{0}\"{2}>@import url({1});</style>";
+		private const string ScriptInclude = "<script type=\"{0}\" src=\"{1}\"{2}></script>";
 
 		#endregion Constants
 
@@ -28,6 +37,8 @@ namespace JsonFx.Client
 		private bool isDebug;
 		private string sourceUrl;
 		private bool usePageCulture = true;
+		private StyleIncludeType styleFormat = StyleIncludeType.Import;
+		private Dictionary<string, string> attributes = null;
 
 		#endregion Fields
 
@@ -76,10 +87,22 @@ namespace JsonFx.Client
 		/// Gets and sets if page determines the culture or
 		/// if uses CurrentUICulture
 		/// </summary>
+		[DefaultValue(true)]
 		public bool UsePageCulture
 		{
 			get { return this.usePageCulture; }
 			set { this.usePageCulture = value; }
+		}
+
+		/// <summary>
+		/// Gets and sets if page determines the culture or
+		/// if uses CurrentUICulture
+		/// </summary>
+		[DefaultValue(StyleIncludeType.Import)]
+		public StyleIncludeType StyleFormat
+		{
+			get { return this.styleFormat; }
+			set { this.styleFormat = value; }
 		}
 
 		#endregion Properties
@@ -105,20 +128,30 @@ namespace JsonFx.Client
 				String.IsNullOrEmpty(info.ContentType) ?
 				String.Empty :
 				info.ContentType.ToLowerInvariant();
+			string attrib = this.BuildCustomAttributes();
+			string format;
 
 			switch (type)
 			{
 				case CssResourceCodeProvider.MimeType:
 				{
-					this.RenderStyleImport(writer, url, info.ContentType);
+					if (this.styleFormat == StyleIncludeType.Import)
+					{
+						format = ResourceInclude.StyleImport;
+					}
+					else
+					{
+						format = ResourceInclude.StyleLink;
+					}
 					break;
 				}
 				default:
 				{
-					this.RenderScriptInclude(writer, url, info.ContentType);
+					format = ResourceInclude.ScriptInclude;
 					break;
 				}
 			}
+			writer.Write(format, type, url, attrib);
 
 			if (info is GlobalizedCompiledBuildResult)
 			{
@@ -128,20 +161,57 @@ namespace JsonFx.Client
 
 				url = ResourceHandler.GetLocalizationUrl(this.SourceUrl, this.isDebug, culture);
 				url = this.ResolveUrl(url);
-				this.RenderScriptInclude(writer, url, ScriptResourceCodeProvider.MimeType);
+				writer.Write(ResourceInclude.ScriptInclude, ScriptResourceCodeProvider.MimeType, url, String.Empty);
 			}
 		}
 
-		private void RenderStyleImport(HtmlTextWriter writer, string url, string mimeType)
-		{
-			writer.Write(StyleImport, mimeType, url);
-		}
-
-		private void RenderScriptInclude(HtmlTextWriter writer, string url, string mimeType)
-		{
-			writer.Write(ScriptInclude, mimeType, url);
-		}
-
 		#endregion Page Event Handlers
+
+		#region Attribute Methods
+
+		private string BuildCustomAttributes()
+		{
+			if (this.attributes == null || this.attributes.Count == 0)
+			{
+				return String.Empty;
+			}
+
+			StringBuilder builder = new StringBuilder();
+			foreach (string key in this.attributes.Keys)
+			{
+				builder.Append(' ');
+				builder.Append(HttpUtility.HtmlAttributeEncode(key.ToLowerInvariant()));
+				builder.Append("=\"");
+				builder.Append(HttpUtility.HtmlAttributeEncode(this.attributes[key]));
+				builder.Append('"');
+			}
+			return builder.ToString();
+		}
+
+		#endregion Attribute Methods
+
+		#region IAttributeAccessor Members
+
+		string IAttributeAccessor.GetAttribute(string key)
+		{
+			if (this.attributes == null || !this.attributes.ContainsKey(key))
+			{
+				return null;
+			}
+
+			return this.attributes[key];
+		}
+
+		void IAttributeAccessor.SetAttribute(string key, string value)
+		{
+			if (this.attributes == null)
+			{
+				this.attributes = new Dictionary<string,string>(2, StringComparer.OrdinalIgnoreCase);
+			}
+
+			this.attributes[key] = value;
+		}
+
+		#endregion IAttributeAccessor Members
 	}
 }
