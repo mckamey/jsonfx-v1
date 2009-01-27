fe3e7d3dc1c3f43218c715b788268714d3184702
Stephen <Stephen@8e687404-24dc-11de-a087-198e6c7179d9>
2009-01-27 16:26:23 +0000

    - improving error messaging when error occurs during type instantiation

--- trunk/JsonFx/JsonFx.IO/Properties/AssemblyVersion.cs
+++ trunk/JsonFx/JsonFx.IO/Properties/AssemblyVersion.cs
@@ -2,10 +2,10 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.901.318
+#region 1.0.901.2709
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.901.318")]
+[assembly: AssemblyVersion("1.0.901.2709")]
 
-#endregion 1.0.901.318
+#endregion 1.0.901.2709
