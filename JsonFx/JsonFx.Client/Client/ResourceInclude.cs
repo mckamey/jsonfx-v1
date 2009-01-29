a46cf97ea360cae944efcdbeb046cbea2b968262
Stephen McKamey <mckamey@users.noreply.github.com>
2009-01-29 05:02:22 +0000

    - fixing long-standing caching bug whereby .NET added "Vary: *" header - adding ability to specify resource types which aren't found by BuildManager - reduced one BuildManager instantiation

--- JsonFx/JsonFx.Client/Client/ResourceInclude.cs
+++ JsonFx/JsonFx.Client/Client/ResourceInclude.cs
@@ -1,3 +1,33 @@
+#region License
+/*---------------------------------------------------------------------------------*\
+
+	Distributed under the terms of an MIT-style license:
+
+	The MIT License
+
+	Copyright (c) 2006-2009 Stephen M. McKamey
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+
+\*---------------------------------------------------------------------------------*/
+#endregion License
+
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -138,22 +168,27 @@ namespace JsonFx.Client
 			CompiledBuildResult info = CompiledBuildResult.Create(url);
 			if (info == null)
 			{
-				throw new ArgumentException(String.Format(
-					"Error loading resources for \"{0}\".\r\n"+
-					"This can be caused by an invalid path, build errors, or incorrect configuration.\r\n"+
-					"Check http://help.jsonfx.net/instructions for troubleshooting.",
-					url));
+				info = this.ResolveUnknownResource(url);
+				if (info == null)
+				{
+					throw new ArgumentException(String.Format(
+						"Error loading resources for \"{0}\".\r\n"+
+						"This can be caused by an invalid path, build errors, or incorrect configuration.\r\n"+
+						"Check http://help.jsonfx.net/instructions for troubleshooting.",
+						url));
+				}
 			}
 
-			url = ResourceHandler.GetResourceUrl(url, this.isDebug);
+			url = ResourceHandler.GetResourceUrl(info, url, this.isDebug);
 			url = this.ResolveUrl(url);
 			string type =
 				String.IsNullOrEmpty(info.ContentType) ?
 				String.Empty :
 				info.ContentType.ToLowerInvariant();
-			string attrib = this.BuildCustomAttributes();
-			string format;
 
+			string attrib = this.BuildCustomAttributes();
+
+			string format;
 			switch (type)
 			{
 				case CssResourceCodeProvider.MimeType:
@@ -186,6 +221,11 @@ namespace JsonFx.Client
 				url = this.ResolveUrl(url);
 				writer.Write(ResourceInclude.ScriptInclude, ScriptResourceCodeProvider.MimeType, url, String.Empty);
 			}
+		}
+
+		protected virtual CompiledBuildResult ResolveUnknownResource(string url)
+		{
+			return null;
 		}
 
 		#endregion Page Event Handlers
