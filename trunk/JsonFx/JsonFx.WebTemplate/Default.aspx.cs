f89b0a0752677c4d350ce24f6d7c7293b9cc94f5
Stephen <Stephen@8e687404-24dc-11de-a087-198e6c7179d9>
2009-01-30 07:22:01 +0000

    - removing auto-insertion of JsonFx version into compacted scripts - changed more conditional compilation blocks to HttpContext.IsDebuggingEnabled

--- trunk/JsonFx/JsonFx.WebTemplate/Default.aspx.cs
+++ trunk/JsonFx/JsonFx.WebTemplate/Default.aspx.cs
@@ -6,10 +6,12 @@ public partial class _Default : System.Web.UI.Page
     {
 		this.PageData["Example.renderTime"] = DateTime.Now;
 		this.PageData["Example.serverName"] = this.Server.MachineName;
+		this.PageData["Example.JsonFxVersion"] = JsonFx.About.Fx.Version;
 
-#if !DEBUG
-		// improve the Yslow rating
-		JsonFx.Handlers.CompiledBuildResult.EnableStreamCompression(this.Context);
-#endif
+		if (!this.Context.IsDebuggingEnabled)
+		{
+			// improve the Yslow rating
+			JsonFx.Handlers.CompiledBuildResult.EnableStreamCompression(this.Context);
+		}
 	}
 }
