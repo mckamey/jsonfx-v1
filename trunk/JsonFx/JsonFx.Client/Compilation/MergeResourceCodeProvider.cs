138f6ec602d5907b63069a01c18f0ee8c4a65625
Stephen <Stephen@8e687404-24dc-11de-a087-198e6c7179d9>
2009-02-02 07:41:19 +0000

    - standardizing app relative path conversion

--- trunk/JsonFx/JsonFx.Client/Compilation/MergeResourceCodeProvider.cs
+++ trunk/JsonFx/JsonFx.Client/Compilation/MergeResourceCodeProvider.cs
@@ -32,6 +32,7 @@ using System;
 using System.IO;
 using System.Collections;
 using System.Collections.Generic;
+using System.Web.Hosting;
 using System.Web.Compilation;
 using System.CodeDom;
 using System.CodeDom.Compiler;
@@ -126,8 +127,15 @@ namespace JsonFx.Compilation
 						continue;
 					}
 
+					// TODO: consolidate app relative path conversion
 					// ensure app-relative BuildManager paths
-					if (files[i].StartsWith("/"))
+					string appRoot = HostingEnvironment.ApplicationVirtualPath;
+					if (appRoot != null && appRoot.Length > 1 &&
+						files[i].StartsWith(appRoot, StringComparison.OrdinalIgnoreCase))
+					{
+						files[i] = "~"+files[i].Substring(appRoot.Length);
+					}
+					else if (files[i].StartsWith("/"))
 					{
 						files[i] = "~"+files[i];
 					}
