6fabdcc80e7b89c3aad75579a481485313448793
Stephen McKamey <mckamey@users.noreply.github.com>
2009-02-26 04:22:57 +0000

    - removing VSS bindings

--- JsonFx/JsonFx.Json.UnitTests/Properties/AssemblyVersion.cs
+++ JsonFx/JsonFx.Json.UnitTests/Properties/AssemblyVersion.cs
@@ -2,10 +2,10 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.901.2710
+#region 1.0.902.2520
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.901.2710")]
+[assembly: AssemblyVersion("1.0.902.2520")]
 
-#endregion 1.0.901.2710
+#endregion 1.0.902.2520
