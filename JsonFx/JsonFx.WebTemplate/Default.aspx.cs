91dffbc29628993b850956e4f99ee5b1d82ecb00
Stephen McKamey <mckamey@users.noreply.github.com>
2009-03-02 01:13:17 +0000

    - simplifying examples

--- JsonFx/JsonFx.WebTemplate/Default.aspx.cs
+++ JsonFx/JsonFx.WebTemplate/Default.aspx.cs
@@ -4,9 +4,9 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-		this.PageData["Example.renderTime"] = DateTime.Now;
-		this.PageData["Example.serverName"] = this.Server.MachineName;
-		this.PageData["Example.JsonFxVersion"] = JsonFx.About.Fx.Version;
+		this.PageData["Example.myData.renderTime"] = DateTime.Now;
+		this.PageData["Example.myData.serverName"] = this.Server.MachineName;
+		this.PageData["Example.myData.JsonFxVersion"] = JsonFx.About.Fx.Version;
 
 		if (!this.Context.IsDebuggingEnabled)
 		{
