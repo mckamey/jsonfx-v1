1b9194d348bf64e3b3e7ebd810c51deefc0945e7
Stephen McKamey <mckamey@users.noreply.github.com>
2009-03-04 17:08:02 +0000

    - releasing v1.0.0903.0408

--- JsonFx/JsonFx.Client/Properties/AssemblyVersion.cs
+++ JsonFx/JsonFx.Client/Properties/AssemblyVersion.cs
@@ -2,10 +2,10 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.903.309
+#region 1.0.903.408
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.903.309")]
+[assembly: AssemblyVersion("1.0.903.408")]
 
-#endregion 1.0.903.309
+#endregion 1.0.903.408
