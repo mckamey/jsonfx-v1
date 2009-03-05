c6f86db920b35c3f22c0a0933d81fd8f89e1ba19
smm <smm@8e687404-24dc-11de-a087-198e6c7179d9>
2009-03-05 17:47:15 +0000

    - released v1.0.0903.0509

--- trunk/JsonFx/JsonFx.History/Properties/AssemblyVersion.cs
+++ trunk/JsonFx/JsonFx.History/Properties/AssemblyVersion.cs
@@ -2,10 +2,10 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.903.408
+#region 1.0.903.509
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.903.408")]
+[assembly: AssemblyVersion("1.0.903.509")]
 
-#endregion 1.0.903.408
+#endregion 1.0.903.509
