28eed92424906b48b40009170425684b0bbcc14b
jsonfx@jsonfx.net <jsonfx@jsonfx.net@8e687404-24dc-11de-a087-198e6c7179d9>
2009-03-12 16:33:35 +0000

    - released v1.0.0903.1209

--- JsonFx/JsonFx.BuildTools/Properties/AssemblyVersion.cs
+++ JsonFx/JsonFx.BuildTools/Properties/AssemblyVersion.cs
@@ -2,10 +2,11 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.903.714
+#region 1.0.903.1209
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.903.714")]
+[assembly: AssemblyVersion("1.0.903.1209")]
+[assembly: AssemblyFileVersion("1.0.903.1209")]
 
-#endregion 1.0.903.714
+#endregion 1.0.903.1209
