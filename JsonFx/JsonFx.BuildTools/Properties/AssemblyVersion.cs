b04296c202f096bedf3213661ae6a68d8c704954
Stephen McKamey <mckamey@users.noreply.github.com>
2009-05-19 16:25:23 +0000

    - releasing v1.0.0905.1909

--- JsonFx/JsonFx.BuildTools/Properties/AssemblyVersion.cs
+++ JsonFx/JsonFx.BuildTools/Properties/AssemblyVersion.cs
@@ -2,11 +2,11 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.905.1309
+#region 1.0.905.1909
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.905.1309")]
-[assembly: AssemblyFileVersion("1.0.905.1309")]
+[assembly: AssemblyVersion("1.0.905.1909")]
+[assembly: AssemblyFileVersion("1.0.905.1909")]
 
-#endregion 1.0.905.1309
+#endregion 1.0.905.1909
