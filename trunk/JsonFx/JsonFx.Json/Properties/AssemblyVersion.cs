f40a99479a01be04e5059e418f31c46832a15ce9
smm <smm@8e687404-24dc-11de-a087-198e6c7179d9>
2009-05-23 14:56:50 +0000

    - releasing v1.0.0905.2307

--- trunk/JsonFx/JsonFx.Json/Properties/AssemblyVersion.cs
+++ trunk/JsonFx/JsonFx.Json/Properties/AssemblyVersion.cs
@@ -2,11 +2,11 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.905.2108
+#region 1.0.905.2307
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.905.2108")]
-[assembly: AssemblyFileVersion("1.0.905.2108")]
+[assembly: AssemblyVersion("1.0.905.2307")]
+[assembly: AssemblyFileVersion("1.0.905.2307")]
 
-#endregion 1.0.905.2108
+#endregion 1.0.905.2307
