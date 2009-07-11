a2058c499b19c15a691b230e87155a2ff05c7912
Stephen McKamey <mckamey@users.noreply.github.com>
2009-07-11 21:02:40 +0000

    - reorganizing structure

---
+++ JbstTools/JbstCompiler/Properties/AssemblyVersion.cs
@@ -0,0 +1,11 @@
+/*------------------------------------------------------------------*\
+	This is auto-generated code.  All changes will be overwritten.
+\*------------------------------------------------------------------*/
+
+#region 1.0.903.810
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.0.903.810")]
+
+#endregion 1.0.903.810
