021691990ff064885ccb8d1fe8632d2b9443490d
jsonfx@jsonfx.net <jsonfx@jsonfx.net@8e687404-24dc-11de-a087-198e6c7179d9>
2009-07-26 23:26:28 +0000

    - first v1.1 release - successful migration to YuiCompressor.NET / EcmaScript.NET

--- JsonFx/JsonFx.History/Properties/AssemblyVersion.cs
+++ JsonFx/JsonFx.History/Properties/AssemblyVersion.cs
@@ -2,11 +2,11 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.907.109
+#region 1.1.907.2616
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.907.109")]
-[assembly: AssemblyFileVersion("1.0.907.109")]
+[assembly: AssemblyVersion("1.1.907.2616")]
+[assembly: AssemblyFileVersion("1.1.907.2616")]
 
-#endregion 1.0.907.109
+#endregion 1.1.907.2616
