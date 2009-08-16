56be34096e1db074706ce5e13ca6fddf918e5a39
smm <smm@8e687404-24dc-11de-a087-198e6c7179d9>
2009-08-16 21:23:15 +0000

    - making build for v1.1.0908.1614

--- trunk/JsonFx/JsonFx.Client/Properties/AssemblyVersion.cs
+++ trunk/JsonFx/JsonFx.Client/Properties/AssemblyVersion.cs
@@ -2,11 +2,11 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.1.908.1408
+#region 1.1.908.1614
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.1.908.1408")]
-[assembly: AssemblyFileVersion("1.1.908.1408")]
+[assembly: AssemblyVersion("1.1.908.1614")]
+[assembly: AssemblyFileVersion("1.1.908.1614")]
 
-#endregion 1.1.908.1408
+#endregion 1.1.908.1614
