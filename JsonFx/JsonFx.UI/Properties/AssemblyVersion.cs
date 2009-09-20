32ad2ac3338452136fd8cfa130a9e49d20c7c6e7
Stephen McKamey <mckamey@users.noreply.github.com>
2009-09-20 18:42:32 +0000

    - making build for v1.1.0909.2011

--- JsonFx/JsonFx.UI/Properties/AssemblyVersion.cs
+++ JsonFx/JsonFx.UI/Properties/AssemblyVersion.cs
@@ -2,11 +2,11 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.1.909.1910
+#region 1.1.909.2011
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.1.909.1910")]
-[assembly: AssemblyFileVersion("1.1.909.1910")]
+[assembly: AssemblyVersion("1.1.909.2011")]
+[assembly: AssemblyFileVersion("1.1.909.2011")]
 
-#endregion 1.1.909.1910
+#endregion 1.1.909.2011
