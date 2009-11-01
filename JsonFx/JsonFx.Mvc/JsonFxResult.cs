8b79f2bd97dad9e7220035260a3d0227d20b1530
Stephen McKamey <mckamey@users.noreply.github.com>
2009-11-01 23:55:03 +0000

    - adding standard license - setting up assembly signing and documentation output

--- JsonFx/JsonFx.Mvc/JsonFxResult.cs
+++ JsonFx/JsonFx.Mvc/JsonFxResult.cs
@@ -1,27 +1,70 @@
-﻿using System;
+﻿#region License
+/*---------------------------------------------------------------------------------*\
+
+	Distributed under the terms of an MIT-style license:
+
+	The MIT License
+
+	Copyright (c) 2006-2009 Stephen M. McKamey
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+
+\*---------------------------------------------------------------------------------*/
+#endregion License
+
+using System;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
+
 using JsonFx.Json;
 
 namespace JsonFx.Mvc
 {
+	/// <summary>
+	/// ActionResult which uses JsonFx to serialize result data as JSON
+	/// </summary>
 	public class JsonFxResult : ActionResult
 	{
 		#region Properties
 
+		/// <summary>
+		/// Gets and sets the content encoding of the response
+		/// </summary>
 		public Encoding ContentEncoding
 		{
 			get;
 			set;
 		}
 
+		/// <summary>
+		/// Gets and sets the content type of the response
+		/// </summary>
 		public string ContentType
 		{
 			get;
 			set;
 		}
 
+		/// <summary>
+		/// Gets and sets the response data to be serialized
+		/// </summary>
 		public object Data
 		{
 			get;
@@ -32,6 +75,10 @@ namespace JsonFx.Mvc
 
 		#region Methods
 
+		/// <summary>
+		/// Renders the actual response
+		/// </summary>
+		/// <param name="context"></param>
 		public override void ExecuteResult(ControllerContext context)
 		{
 			if (context == null)
