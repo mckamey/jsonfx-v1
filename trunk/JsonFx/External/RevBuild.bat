9597cd83d3e846afa771ed8ff6fd2dc085ce901b
smm <smm@8e687404-24dc-11de-a087-198e6c7179d9>
2009-11-01 23:33:31 +0000

    - adding VS2008 solution to make maintaining both versions easier

--- trunk/JsonFx/External/RevBuild.bat
+++ trunk/JsonFx/External/RevBuild.bat
@@ -19,6 +19,8 @@ IF NOT EXIST %AutoVer% (
 
 %AutoVer% "%SolnDir%JsonFx.Json\Properties\AssemblyVersion.cs" %BaseVersion%
 
+%AutoVer% "%SolnDir%JsonFx.Json.UnitTests\Properties\AssemblyVersion.cs" %BaseVersion%
+
 %AutoVer% "%SolnDir%JsonFx.UI\Properties\AssemblyVersion.cs" %BaseVersion%
 
 :Done
