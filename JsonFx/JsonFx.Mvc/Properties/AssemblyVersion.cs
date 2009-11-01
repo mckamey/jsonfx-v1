452eda8bb01afdfd617b47d687645e261be95c57
Stephen McKamey <mckamey@users.noreply.github.com>
2009-11-01 23:45:33 +0000

    - adding placeholder JsonFx.Mvc project

---
+++ JsonFx/JsonFx.Mvc/Properties/AssemblyVersion.cs
@@ -0,0 +1,12 @@
+/*------------------------------------------------------------------*\
+	This is auto-generated code.  All changes will be overwritten.
+\*------------------------------------------------------------------*/
+
+#region 1.1.910.3107
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.1.910.3107")]
+[assembly: AssemblyFileVersion("1.1.910.3107")]
+
+#endregion 1.1.910.3107
