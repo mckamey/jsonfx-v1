01ca508abbef97c09559e6b7bcc4d297421a2c98
Stephen McKamey <mckamey@users.noreply.github.com>
2009-11-26 06:41:24 +0000

    - updating to JsonFx 1.3.0911.2522

--- JbstOnline/MvcApp/Properties/AssemblyVersion.cs
+++ JbstOnline/MvcApp/Properties/AssemblyVersion.cs
@@ -2,11 +2,11 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.910.3107
+#region 1.0.911.2522
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.910.3107")]
-[assembly: AssemblyFileVersion("1.0.910.3107")]
+[assembly: AssemblyVersion("1.0.911.2522")]
+[assembly: AssemblyFileVersion("1.0.911.2522")]
 
-#endregion 1.0.910.3107
+#endregion 1.0.911.2522
