b05e472f0c0876610f3dd0dfb12aa9b0f7eede64
smm <smm@8e687404-24dc-11de-a087-198e6c7179d9>
2009-11-28 19:57:52 +0000

    - controller clean up - added view-model for Home/Index - moved more logic into reusable components

--- trunk/JbstOnline/MvcApp/Controllers/ExampleController.cs
+++ trunk/JbstOnline/MvcApp/Controllers/ExampleController.cs
@@ -1,18 +1,13 @@
 ﻿using System;
-using System.Web;
-using System.Web.Compilation;
 using System.Web.Mvc;
-
-using JsonFx.Handlers;
 
 namespace JbstOnline.Controllers
 {
-	[HandleError]
 	public class ExampleController : AppControllerBase
 	{
 		public ActionResult Index()
 		{
-			return new ViewResult();
+			return this.View();
 		}
 	}
 }
