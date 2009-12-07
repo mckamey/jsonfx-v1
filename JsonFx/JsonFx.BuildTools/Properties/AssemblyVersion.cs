15d3b077f58d7de83f614fc1429b64aaeabfce22
Stephen McKamey <mckamey@users.noreply.github.com>
2009-12-07 04:38:31 +0000

    - v1.3.0912.0620

--- JsonFx/JsonFx.BuildTools/Properties/AssemblyVersion.cs
+++ JsonFx/JsonFx.BuildTools/Properties/AssemblyVersion.cs
@@ -2,11 +2,11 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.3.911.2815
+#region 1.3.912.620
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.3.911.2815")]
-[assembly: AssemblyFileVersion("1.3.911.2815")]
+[assembly: AssemblyVersion("1.3.912.620")]
+[assembly: AssemblyFileVersion("1.3.912.620")]
 
-#endregion 1.3.911.2815
+#endregion 1.3.912.620
