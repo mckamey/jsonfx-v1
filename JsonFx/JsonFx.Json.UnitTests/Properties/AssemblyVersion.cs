f537c212b9e4b711db799be9fe74092bd6c72d62
Stephen McKamey <mckamey@users.noreply.github.com>
2010-01-12 14:08:09 +0000

    - v1.3.1001.1206

--- JsonFx/JsonFx.Json.UnitTests/Properties/AssemblyVersion.cs
+++ JsonFx/JsonFx.Json.UnitTests/Properties/AssemblyVersion.cs
@@ -2,11 +2,11 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.3.1001.608
+#region 1.3.1001.1206
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.3.1001.608")]
-[assembly: AssemblyFileVersion("1.3.1001.608")]
+[assembly: AssemblyVersion("1.3.1001.1206")]
+[assembly: AssemblyFileVersion("1.3.1001.1206")]
 
-#endregion 1.3.1001.608
+#endregion 1.3.1001.1206
