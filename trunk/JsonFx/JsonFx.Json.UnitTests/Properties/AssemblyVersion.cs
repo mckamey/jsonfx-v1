aadb101e306fdc69c8285e9716fa5b6b0bbb2f9e
smm <smm@8e687404-24dc-11de-a087-198e6c7179d9>
2010-03-08 17:00:02 +0000

    - v1.3.1003.0808

--- trunk/JsonFx/JsonFx.Json.UnitTests/Properties/AssemblyVersion.cs
+++ trunk/JsonFx/JsonFx.Json.UnitTests/Properties/AssemblyVersion.cs
@@ -2,11 +2,11 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.3.1001.2320
+#region 1.3.1003.808
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.3.1001.2320")]
-[assembly: AssemblyFileVersion("1.3.1001.2320")]
+[assembly: AssemblyVersion("1.3.1003.808")]
+[assembly: AssemblyFileVersion("1.3.1003.808")]
 
-#endregion 1.3.1001.2320
+#endregion 1.3.1003.808
